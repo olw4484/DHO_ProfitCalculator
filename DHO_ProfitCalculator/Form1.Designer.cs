26024b925cee91b7b2aa643fb917f4bf19314264
owl4484 <rodeo23267286@gmail.com>
2025-04-12 22:41:01 +0900

    feat.발주서값 및 재료값 계산기능 구현
    총 비용 계산을 보여주는 textbox 추가 및 예상 생산 갯수 계산 및 수익 계산기능 추가할 필요가 있음.

--- DHO_ProfitCalculator/Form1.Designer.cs
+++ DHO_ProfitCalculator/Form1.Designer.cs
@@ -28,12 +28,530 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            buttonCalculateOrders = new Button();
+            textBox1 = new TextBox();
+            label1 = new Label();
+            textBox2 = new TextBox();
+            label2 = new Label();
+            textBox4 = new TextBox();
+            label3 = new Label();
+            label4 = new Label();
+            textBox5 = new TextBox();
+            textBox6 = new TextBox();
+            textBox8 = new TextBox();
+            label5 = new Label();
+            label6 = new Label();
+            textBox9 = new TextBox();
+            textBox10 = new TextBox();
+            textBox12 = new TextBox();
+            label7 = new Label();
+            label8 = new Label();
+            textBox13 = new TextBox();
+            textBox14 = new TextBox();
+            textBox16 = new TextBox();
+            label9 = new Label();
+            label10 = new Label();
+            label11 = new Label();
+            label12 = new Label();
+            label13 = new Label();
+            label14 = new Label();
+            label15 = new Label();
+            label16 = new Label();
+            textBox17 = new TextBox();
+            textBox18 = new TextBox();
+            textBox19 = new TextBox();
+            label17 = new Label();
+            textBox20 = new TextBox();
+            label18 = new Label();
+            textBox21 = new TextBox();
+            textBox22 = new TextBox();
+            label19 = new Label();
+            label20 = new Label();
+            label21 = new Label();
+            textBox23 = new TextBox();
+            textBox24 = new TextBox();
+            textBox25 = new TextBox();
+            label22 = new Label();
+            buttonCalculateMaterials = new Button();
+            buttonCalculateTotal = new Button();
+            SuspendLayout();
+            // 
+            // buttonCalculateOrders
+            // 
+            buttonCalculateOrders.Location = new Point(467, 361);
+            buttonCalculateOrders.Name = "buttonCalculateOrders";
+            buttonCalculateOrders.Size = new Size(100, 77);
+            buttonCalculateOrders.TabIndex = 0;
+            buttonCalculateOrders.Text = "발주서 값 계산하기(F2)";
+            buttonCalculateOrders.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(109, 29);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(148, 27);
+            textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(21, 32);
+            label1.Name = "label1";
+            label1.Size = new Size(82, 20);
+            label1.TabIndex = 2;
+            label1.Text = "1번 발주서";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(263, 29);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(164, 27);
+            textBox2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(447, 36);
+            label2.Name = "label2";
+            label2.Size = new Size(132, 20);
+            label2.TabIndex = 5;
+            label2.Text = "1번 발주서 소모값";
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(641, 32);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(100, 27);
+            textBox4.TabIndex = 6;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(21, 65);
+            label3.Name = "label3";
+            label3.Size = new Size(82, 20);
+            label3.TabIndex = 7;
+            label3.Text = "2번 발주서";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(447, 65);
+            label4.Name = "label4";
+            label4.Size = new Size(132, 20);
+            label4.TabIndex = 8;
+            label4.Text = "2번 발주서 소모값";
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(109, 62);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(148, 27);
+            textBox5.TabIndex = 9;
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(263, 62);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(164, 27);
+            textBox6.TabIndex = 10;
+            // 
+            // textBox8
+            // 
+            textBox8.Location = new Point(641, 65);
+            textBox8.Name = "textBox8";
+            textBox8.Size = new Size(100, 27);
+            textBox8.TabIndex = 12;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(21, 98);
+            label5.Name = "label5";
+            label5.Size = new Size(82, 20);
+            label5.TabIndex = 13;
+            label5.Text = "3번 발주서";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(447, 95);
+            label6.Name = "label6";
+            label6.Size = new Size(132, 20);
+            label6.TabIndex = 14;
+            label6.Text = "3번 발주서 소모값";
+            // 
+            // textBox9
+            // 
+            textBox9.Location = new Point(109, 95);
+            textBox9.Name = "textBox9";
+            textBox9.Size = new Size(148, 27);
+            textBox9.TabIndex = 15;
+            // 
+            // textBox10
+            // 
+            textBox10.Location = new Point(263, 95);
+            textBox10.Name = "textBox10";
+            textBox10.Size = new Size(164, 27);
+            textBox10.TabIndex = 16;
+            // 
+            // textBox12
+            // 
+            textBox12.Location = new Point(641, 95);
+            textBox12.Name = "textBox12";
+            textBox12.Size = new Size(100, 27);
+            textBox12.TabIndex = 18;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(21, 132);
+            label7.Name = "label7";
+            label7.Size = new Size(82, 20);
+            label7.TabIndex = 19;
+            label7.Text = "4번 발주서";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(447, 125);
+            label8.Name = "label8";
+            label8.Size = new Size(132, 20);
+            label8.TabIndex = 20;
+            label8.Text = "4번 발주서 소모값";
+            // 
+            // textBox13
+            // 
+            textBox13.Location = new Point(109, 125);
+            textBox13.Name = "textBox13";
+            textBox13.Size = new Size(148, 27);
+            textBox13.TabIndex = 21;
+            // 
+            // textBox14
+            // 
+            textBox14.Location = new Point(263, 125);
+            textBox14.Name = "textBox14";
+            textBox14.Size = new Size(164, 27);
+            textBox14.TabIndex = 22;
+            // 
+            // textBox16
+            // 
+            textBox16.Location = new Point(641, 125);
+            textBox16.Name = "textBox16";
+            textBox16.Size = new Size(100, 27);
+            textBox16.TabIndex = 24;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(21, 6);
+            label9.Name = "label9";
+            label9.Size = new Size(89, 20);
+            label9.TabIndex = 25;
+            label9.Text = "발주서 종류";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Location = new Point(116, 6);
+            label10.Name = "label10";
+            label10.Size = new Size(89, 20);
+            label10.TabIndex = 26;
+            label10.Text = "발주서 가격";
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Location = new Point(266, 6);
+            label11.Name = "label11";
+            label11.Size = new Size(119, 20);
+            label11.TabIndex = 27;
+            label11.Text = "발주서 사용갯수";
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.Location = new Point(468, 6);
+            label12.Name = "label12";
+            label12.Size = new Size(89, 20);
+            label12.TabIndex = 28;
+            label12.Text = "발주서 계산";
+            // 
+            // label13
+            // 
+            label13.AutoSize = true;
+            label13.Location = new Point(641, 6);
+            label13.Name = "label13";
+            label13.Size = new Size(59, 20);
+            label13.TabIndex = 29;
+            label13.Text = "총 비용";
+            // 
+            // label14
+            // 
+            label14.AutoSize = true;
+            label14.Location = new Point(21, 186);
+            label14.Name = "label14";
+            label14.Size = new Size(82, 20);
+            label14.TabIndex = 30;
+            label14.Text = "1번째 재료";
+            // 
+            // label15
+            // 
+            label15.AutoSize = true;
+            label15.Location = new Point(23, 219);
+            label15.Name = "label15";
+            label15.Size = new Size(82, 20);
+            label15.TabIndex = 31;
+            label15.Text = "2번째 재료";
+            // 
+            // label16
+            // 
+            label16.AutoSize = true;
+            label16.Location = new Point(23, 252);
+            label16.Name = "label16";
+            label16.Size = new Size(82, 20);
+            label16.TabIndex = 32;
+            label16.Text = "3번째 재료";
+            // 
+            // textBox17
+            // 
+            textBox17.Location = new Point(111, 183);
+            textBox17.Name = "textBox17";
+            textBox17.Size = new Size(82, 27);
+            textBox17.TabIndex = 33;
+            // 
+            // textBox18
+            // 
+            textBox18.Location = new Point(111, 216);
+            textBox18.Name = "textBox18";
+            textBox18.Size = new Size(82, 27);
+            textBox18.TabIndex = 34;
+            // 
+            // textBox19
+            // 
+            textBox19.Location = new Point(111, 249);
+            textBox19.Name = "textBox19";
+            textBox19.Size = new Size(82, 27);
+            textBox19.TabIndex = 35;
+            // 
+            // label17
+            // 
+            label17.AutoSize = true;
+            label17.Location = new Point(116, 160);
+            label17.Name = "label17";
+            label17.Size = new Size(39, 20);
+            label17.TabIndex = 36;
+            label17.Text = "수량";
+            // 
+            // textBox20
+            // 
+            textBox20.Location = new Point(199, 183);
+            textBox20.Name = "textBox20";
+            textBox20.Size = new Size(80, 27);
+            textBox20.TabIndex = 37;
+            // 
+            // label18
+            // 
+            label18.AutoSize = true;
+            label18.Location = new Point(204, 159);
+            label18.Name = "label18";
+            label18.Size = new Size(39, 20);
+            label18.TabIndex = 38;
+            label18.Text = "가격";
+            // 
+            // textBox21
+            // 
+            textBox21.Location = new Point(199, 216);
+            textBox21.Name = "textBox21";
+            textBox21.Size = new Size(80, 27);
+            textBox21.TabIndex = 39;
+            // 
+            // textBox22
+            // 
+            textBox22.Location = new Point(199, 249);
+            textBox22.Name = "textBox22";
+            textBox22.Size = new Size(80, 27);
+            textBox22.TabIndex = 40;
+            // 
+            // label19
+            // 
+            label19.AutoSize = true;
+            label19.Location = new Point(285, 186);
+            label19.Name = "label19";
+            label19.Size = new Size(117, 20);
+            label19.TabIndex = 41;
+            label19.Text = "1번째 재료 비용";
+            // 
+            // label20
+            // 
+            label20.AutoSize = true;
+            label20.Location = new Point(285, 219);
+            label20.Name = "label20";
+            label20.Size = new Size(117, 20);
+            label20.TabIndex = 42;
+            label20.Text = "2번째 재료 비용";
+            // 
+            // label21
+            // 
+            label21.AutoSize = true;
+            label21.Location = new Point(285, 252);
+            label21.Name = "label21";
+            label21.Size = new Size(117, 20);
+            label21.TabIndex = 43;
+            label21.Text = "3번째 재료 비용";
+            // 
+            // textBox23
+            // 
+            textBox23.Location = new Point(408, 183);
+            textBox23.Name = "textBox23";
+            textBox23.Size = new Size(100, 27);
+            textBox23.TabIndex = 44;
+            // 
+            // textBox24
+            // 
+            textBox24.Location = new Point(408, 216);
+            textBox24.Name = "textBox24";
+            textBox24.Size = new Size(100, 27);
+            textBox24.TabIndex = 45;
+            // 
+            // textBox25
+            // 
+            textBox25.Location = new Point(408, 249);
+            textBox25.Name = "textBox25";
+            textBox25.Size = new Size(100, 27);
+            textBox25.TabIndex = 46;
+            // 
+            // label22
+            // 
+            label22.AutoSize = true;
+            label22.Location = new Point(408, 160);
+            label22.Name = "label22";
+            label22.Size = new Size(94, 20);
+            label22.TabIndex = 47;
+            label22.Text = "재료 총 비용";
+            // 
+            // buttonCalculateMaterials
+            // 
+            buttonCalculateMaterials.Location = new Point(573, 360);
+            buttonCalculateMaterials.Name = "buttonCalculateMaterials";
+            buttonCalculateMaterials.Size = new Size(100, 77);
+            buttonCalculateMaterials.TabIndex = 48;
+            buttonCalculateMaterials.Text = "재료값 계산하기(F3)";
+            buttonCalculateMaterials.UseVisualStyleBackColor = true;
+            // 
+            // buttonCalculateTotal
+            // 
+            buttonCalculateTotal.Location = new Point(679, 360);
+            buttonCalculateTotal.Name = "buttonCalculateTotal";
+            buttonCalculateTotal.Size = new Size(100, 77);
+            buttonCalculateTotal.TabIndex = 49;
+            buttonCalculateTotal.Text = "총 비용 계산하기(F4)";
+            buttonCalculateTotal.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(9F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(buttonCalculateTotal);
+            Controls.Add(buttonCalculateMaterials);
+            Controls.Add(label22);
+            Controls.Add(textBox25);
+            Controls.Add(textBox24);
+            Controls.Add(textBox23);
+            Controls.Add(label21);
+            Controls.Add(label20);
+            Controls.Add(label19);
+            Controls.Add(textBox22);
+            Controls.Add(textBox21);
+            Controls.Add(label18);
+            Controls.Add(textBox20);
+            Controls.Add(label17);
+            Controls.Add(textBox19);
+            Controls.Add(textBox18);
+            Controls.Add(textBox17);
+            Controls.Add(label16);
+            Controls.Add(label15);
+            Controls.Add(label14);
+            Controls.Add(label13);
+            Controls.Add(label12);
+            Controls.Add(label11);
+            Controls.Add(label10);
+            Controls.Add(label9);
+            Controls.Add(textBox16);
+            Controls.Add(textBox14);
+            Controls.Add(textBox13);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(textBox12);
+            Controls.Add(textBox10);
+            Controls.Add(textBox9);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(textBox8);
+            Controls.Add(textBox6);
+            Controls.Add(textBox5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(textBox4);
+            Controls.Add(label2);
+            Controls.Add(textBox2);
+            Controls.Add(label1);
+            Controls.Add(textBox1);
+            Controls.Add(buttonCalculateOrders);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button buttonCalculateOrders;
+        private TextBox textBox1;
+        private Label label1;
+        private TextBox textBox2;
+        private Label label2;
+        private TextBox textBox4;
+        private Label label3;
+        private Label label4;
+        private TextBox textBox5;
+        private TextBox textBox6;
+        private TextBox textBox8;
+        private Label label5;
+        private Label label6;
+        private TextBox textBox9;
+        private TextBox textBox10;
+        private TextBox textBox12;
+        private Label label7;
+        private Label label8;
+        private TextBox textBox13;
+        private TextBox textBox14;
+        private TextBox textBox16;
+        private Label label9;
+        private Label label10;
+        private Label label11;
+        private Label label12;
+        private Label label13;
+        private Label label14;
+        private Label label15;
+        private Label label16;
+        private TextBox textBox17;
+        private TextBox textBox18;
+        private TextBox textBox19;
+        private Label label17;
+        private TextBox textBox20;
+        private Label label18;
+        private TextBox textBox21;
+        private TextBox textBox22;
+        private Label label19;
+        private Label label20;
+        private Label label21;
+        private TextBox textBox23;
+        private TextBox textBox24;
+        private TextBox textBox25;
+        private Label label22;
+        private Button buttonCalculateMaterials;
+        private Button buttonCalculateTotal;
     }
 }
--- DHO_ProfitCalculator/Form1.cs
+++ DHO_ProfitCalculator/Form1.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Windows.Forms;
+
 namespace DHO_ProfitCalculator
 {
     public partial class Form1 : Form
@@ -5,6 +8,119 @@ namespace DHO_ProfitCalculator
         public Form1()
         {
             InitializeComponent();
+
+            this.KeyPreview = true;
+            this.KeyDown += Form1_KeyDown;
+
+            buttonCalculateOrders.Click += buttonCalculate_Click;
+            buttonCalculateMaterials.Click += buttonCalculateMaterials_Click;
+            buttonCalculateTotal.Click += buttonCalculateTotal_Click;
+
+
         }
+
+        private void buttonCalculate_Click(object? sender, EventArgs e)
+        {
+            try
+            {
+                // null 또는 비어있는 텍스트박스를 0으로 처리
+                double price1 = string.IsNullOrWhiteSpace(textBox1.Text) ? 0 : Convert.ToDouble(textBox1.Text);
+                int quantity1 = string.IsNullOrWhiteSpace(textBox2.Text) ? 0 : Convert.ToInt32(textBox2.Text);
+                double totalCost1 = price1 * quantity1;
+                textBox4.Text = totalCost1.ToString("F2");
+
+                // 2번 발주서
+                double price2 = string.IsNullOrWhiteSpace(textBox5.Text) ? 0 : Convert.ToDouble(textBox5.Text);
+                int quantity2 = string.IsNullOrWhiteSpace(textBox6.Text) ? 0 : Convert.ToInt32(textBox6.Text);
+                double totalCost2 = price2 * quantity2;
+                textBox8.Text = totalCost2.ToString("F2");
+
+                // 3번 발주서
+                double price3 = string.IsNullOrWhiteSpace(textBox9.Text) ? 0 : Convert.ToDouble(textBox9.Text);
+                int quantity3 = string.IsNullOrWhiteSpace(textBox10.Text) ? 0 : Convert.ToInt32(textBox10.Text);
+                double totalCost3 = price3 * quantity3;
+                textBox12.Text = totalCost3.ToString("F2");
+
+                // 4번 발주서
+                double price4 = string.IsNullOrWhiteSpace(textBox13.Text) ? 0 : Convert.ToDouble(textBox13.Text);
+                int quantity4 = string.IsNullOrWhiteSpace(textBox14.Text) ? 0 : Convert.ToInt32(textBox14.Text);
+                double totalCost4 = price4 * quantity4;
+                textBox16.Text = totalCost4.ToString("F2");
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("올바른 숫자를 입력해주세요!", "입력 오류", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+        private void buttonCalculateMaterials_Click(object? sender, EventArgs e)
+        {
+            try
+            {
+                double materialCost1 = (string.IsNullOrWhiteSpace(textBox17.Text) ? 0 : Convert.ToDouble(textBox17.Text)) *
+                                       (string.IsNullOrWhiteSpace(textBox20.Text) ? 0 : Convert.ToDouble(textBox20.Text));
+                double materialCost2 = (string.IsNullOrWhiteSpace(textBox18.Text) ? 0 : Convert.ToDouble(textBox18.Text)) *
+                                       (string.IsNullOrWhiteSpace(textBox21.Text) ? 0 : Convert.ToDouble(textBox21.Text));
+                double materialCost3 = (string.IsNullOrWhiteSpace(textBox19.Text) ? 0 : Convert.ToDouble(textBox19.Text)) *
+                                       (string.IsNullOrWhiteSpace(textBox22.Text) ? 0 : Convert.ToDouble(textBox22.Text));
+
+                textBox23.Text = materialCost1.ToString("F2");
+                textBox24.Text = materialCost2.ToString("F2");
+                textBox25.Text = materialCost3.ToString("F2");
+
+                double totalMaterialCost = materialCost1 + materialCost2 + materialCost3;
+                label22.Text = $"재료 총 비용: {totalMaterialCost:F2}두캇";
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("올바른 숫자를 입력해주세요!", "입력 오류", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+        private void buttonCalculateTotal_Click(object? sender, EventArgs e)
+        {
+            try
+            {
+                // 발주서 총 비용 계산
+                double totalOrderCost = (string.IsNullOrWhiteSpace(textBox4.Text) ? 0 : Convert.ToDouble(textBox4.Text)) +
+                                        (string.IsNullOrWhiteSpace(textBox8.Text) ? 0 : Convert.ToDouble(textBox8.Text)) +
+                                        (string.IsNullOrWhiteSpace(textBox12.Text) ? 0 : Convert.ToDouble(textBox12.Text)) +
+                                        (string.IsNullOrWhiteSpace(textBox16.Text) ? 0 : Convert.ToDouble(textBox16.Text));
+
+                // 재료 총 비용 계산
+                double totalMaterialCost = (string.IsNullOrWhiteSpace(textBox23.Text) ? 0 : Convert.ToDouble(textBox23.Text)) +
+                                           (string.IsNullOrWhiteSpace(textBox24.Text) ? 0 : Convert.ToDouble(textBox24.Text)) +
+                                           (string.IsNullOrWhiteSpace(textBox25.Text) ? 0 : Convert.ToDouble(textBox25.Text));
+
+                // 최종 총 비용 계산
+                double grandTotal = totalOrderCost + totalMaterialCost;
+
+                // 결과 출력
+                MessageBox.Show($"발주서 총 비용: {totalOrderCost:F2}두캇\n재료 총 비용: {totalMaterialCost:F2}두캇\n최종 총 비용: {grandTotal:F2}두캇",
+                                "총합 결과", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("올바른 숫자를 입력해주세요!", "입력 오류", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+        // KeyDown 이벤트 핸들러
+        private void Form1_KeyDown(object? sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.F2)
+            {
+                // F2: 발주서 계산
+                buttonCalculate_Click(sender, e);
+            }
+            else if (e.KeyCode == Keys.F3)
+            {
+                // F3: 재료값 계산
+                buttonCalculateMaterials_Click(sender, e);
+            }
+            else if (e.KeyCode == Keys.F4)
+            {
+                // F4: 비용 계산
+                buttonCalculateTotal_Click(sender, e);
+            }
+        }
+
     }
 }
